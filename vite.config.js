815fa0ef906aa61aa22434d64baf34b9ada124a0
nt-leoleo <pederneraleonardo729@gmail.com>
2025-09-24 19:44:57 -0300

    arreglo de vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/anthem-website/", // 👈 nombre exacto de tu repo en GitHub
+});
